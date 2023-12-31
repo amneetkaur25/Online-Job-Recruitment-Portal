c22e3ab827d1ab93b02a7ae27d74bdb9ed74281d
Amneet Kaur <111182139+amneetkaur25@users.noreply.github.com>
2023-12-31 22:37:48 +0530

    Add files via upload

---
+++ ApplicantViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_Recruitment_Portal.Models.ViewModel
+{
+    public class ApplicantViewModel
+    {
+        public int Id { get; set; }
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "Please enter a valid name with alphabetic characters only.")]
+        [Display(Name = "Applicant Name")]
+        public string ApplicantName { get; set; }
+        [Required(ErrorMessage = "Please upload your resume.")]
+        public IFormFile ResumeFile { get; set; }
+        public DateTime ApplicationDate { get; set; } = DateTime.Now;
+        public int JobPostingId { get; set; }
+    }
+}

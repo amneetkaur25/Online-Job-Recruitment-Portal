c72933c9764730c359136433be42b493d008d1de
Amneet Kaur <111182139+amneetkaur25@users.noreply.github.com>
2023-12-31 22:38:23 +0530

    Add files via upload

---
+++ Applicant.cs
@@ -0,0 +1,35 @@
+﻿using Online_Recruitment_Portal.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Online_Recruitment_Portal.Models
+{
+    public class Applicant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Applicant Name")]
+        public string ApplicantName { get; set; }
+
+        [Required]
+        [Display(Name = "Resume")]
+        public string ResumeFile { get; set; }
+
+        [Display(Name = "Application Date")]
+        public DateTime ApplicationDate { get; set; } = DateTime.Now;
+
+        [Display(Name = "Job Posting")]
+        public int JobPostingId { get; set; }
+
+        [ForeignKey("JobPostingId")]
+        public JobPosting JobPosting { get; set; }
+
+        [Display(Name = "User ID")]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ JobPosting.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_Recruitment_Portal.Models
+{
+    public class JobPosting
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string JobTitle { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string JobDescription { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Display(Name = "Posted Date")]
+        public DateTime PostedDate { get; set; }
+
+        //[Display(Name = "Application Deadline")]
+        //[Required]
+        //public DateTime ApplicationDeadline { get; set; }
+        private DateTime _applicationDeadline;
+        public DateTime ApplicationDeadline
+        {
+            get { return _applicationDeadline; }
+            set
+            {
+                if (IsApplicationDeadlineValid(value))
+                {
+                    _applicationDeadline = value;
+                    ApplicationDeadlineErrorMessage = null; // Clear any previous error message
+                }
+                else
+                {
+                    ApplicationDeadlineErrorMessage = "Application Deadline cannot be earlier than Posted Date.";
+                }
+            }
+        }
+
+        public string ApplicationDeadlineErrorMessage { get; private set; }
+
+        private bool IsApplicationDeadlineValid(DateTime value)
+        {
+            // Ensure that the ApplicationDeadline is not earlier than PostedDate.
+            return value >= PostedDate;
+        }
+
+        // Other properties and methods...
+    
+
+    public JobStatus Status { get; set; } = JobStatus.Pending;
+    }
+
+    public enum JobStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+}
